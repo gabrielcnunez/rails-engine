67dd47e203d32f7a35c4f9d65561cfd2ffef78e6
Gabe Nunez <gabriel.c.nunez@gmail.com>
2022-12-06 14:29:20 -0700

    Comment out relationships to pass postman testing

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -2,5 +2,5 @@ class ItemSerializer
   include JSONAPI::Serializer
   attributes :name, :description, :unit_price, :merchant_id
 
-  belongs_to :merchant
+  # belongs_to :merchant
 end
--- app/serializers/merchant_serializer.rb
+++ app/serializers/merchant_serializer.rb
@@ -2,5 +2,5 @@ class MerchantSerializer
   include JSONAPI::Serializer
   attributes :name
 
-  has_many :items
+  # has_many :items
 end

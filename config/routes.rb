f8579cef75d7a7f5c00ecf6ff0f937e088bad533
Gabe Nunez <gabriel.c.nunez@gmail.com>
2022-12-05 20:34:40 -0700

    Create Item model and set up corresponding factory

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :merchants, only: [:index]
+      resources :items, only: [:index]
     end
   end
 end
---
+++ spec/factories/items.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :item do
+    name { Faker::Beer.brand }
+    description { Faker::Beer.name }
+    unit_price { Faker::Number.between(from: 1.99, to: 9.99) }
+  end
+end

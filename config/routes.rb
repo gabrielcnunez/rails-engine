31f2662b02c47e394ffa70c0f8a0f3f6d26eadac
Gabe Nunez <gabriel.c.nunez@gmail.com>
2022-12-07 15:53:39 -0700

    Pass initial routing testing

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :merchants, only: [:index, :show]
+      resources :merchants, only: [:index, :show] do
+        resources :items, only: [:index], to: 'merchants#all_items'
+      end
       resources :items, only: [:index, :show, :create, :update]
     end
   end

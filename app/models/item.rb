b3eb292fc9dfaea859ed90b814cf966992dfce0e
Gabe Nunez <gabriel.c.nunez@gmail.com>
2022-12-08 16:45:02 -0700

    Create item name search class method, pass testing

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,7 @@
 class Item < ApplicationRecord
   belongs_to :merchant
+
+  def self.name_search(param)
+    where('name ILIKE ?', "%#{param}%").order(:name).first
+  end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -3,3 +3,18 @@ require 'rails_helper'
 RSpec.describe Item, type: :model do
   it { should belong_to :merchant }
 end
+
+describe 'methods' do
+  before :each do
+    @item1 = create(:item, name: 'India Pale Beer', unit_price: 12.99)
+    @item2 = create(:item, name: 'Christmas Ale', unit_price: 8.99)
+    @item3 = create(:item, name: 'Boston Lager', unit_price: 9.99)
+    @item4 = create(:item, name: 'Amber Bock', unit_price: 4.99)
+  end
+
+  describe '#name_search' do
+    it 'returns an item using case insensitive partial match query params' do
+      expect(Item.name_search('ale')).to eq(@item2)
+    end
+  end
+end

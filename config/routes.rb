a41ea181098e7f189e224b99fb3380c345de0301
Gabe Nunez <gabriel.c.nunez@gmail.com>
2022-12-08 12:20:53 -0700

    Complete find all merchants response, pass happy path testing

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
+      namespace :merchants do
+        resources :find_all, only: :index, to: 'search#index'
+      end
       resources :merchants, only: [:index, :show] do
         resources :items, only: [:index], to: 'items#merchant_items'
       end

c028eb1e28bcadad190d8a03b2cbaca4b725ed31
Gabe Nunez <gabriel.c.nunez@gmail.com>
2022-12-08 12:02:27 -0700

    Create merchant search class method, pass testing

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -4,4 +4,16 @@ RSpec.describe Merchant, type: :model do
   describe 'relationships' do
     it { should have_many :items }
   end
+
+  describe 'methods' do
+    describe '#merchant_search' do
+      it 'returns all merchants using case insensitive partial match query params' do
+        merchant1 = create(:merchant, name: "BoJack Horseman")
+        merchant2 = create(:merchant, name: "Jack Shepard")
+        merchant3 = create(:merchant, name: "Audrey Horne")
+
+        expect(Merchant.merchant_search('jack')).to eq([merchant1, merchant2])
+      end
+    end
+  end
 end

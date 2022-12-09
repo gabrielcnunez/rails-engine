86967156e5da7686f002f32ff527cca8d46a3be9
Gabe Nunez <gabriel.c.nunez@gmail.com>
2022-12-08 18:42:00 -0700

    Create item range price search class method, pass testing

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,4 +12,8 @@ class Item < ApplicationRecord
   def self.max_search(param)
     where('unit_price < ?', param).order(:name).first
   end
+
+  def self.range_search(min_param, max_param)
+    where('unit_price > ? AND unit_price < ?', min_param, max_param).order(:name).first
+  end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -10,6 +10,7 @@ describe 'methods' do
     @item2 = create(:item, name: 'Christmas Ale', unit_price: 8.99)
     @item3 = create(:item, name: 'Boston Lager', unit_price: 9.99)
     @item4 = create(:item, name: 'Amber Bock', unit_price: 4.99)
+    @item5 = create(:item, name: 'Winter Ale', unit_price: 13.99)
   end
 
   describe '#name_search' do
@@ -27,6 +28,12 @@ describe 'methods' do
   describe '#max_search' do
     it 'returns an item below a maximum price query param' do
       expect(Item.max_search(9)).to eq(@item4)
+    end
+  end
+
+  describe '#range_search' do
+    it 'returns an item with a range of minimum and maximum price query param' do
+      expect(Item.range_search(10, 15)).to eq(@item1)
     end
   end
 end
